a1d290571aa9fc9ed401eee79fec1cb5d5ad6271
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-20 11:30:31 +0300

    add Auth controller

---
+++ src/main/java/com/teodor/backend/controller/AuthController.java
@@ -0,0 +1,56 @@
+package com.teodor.backend.controller;
+
+import com.teodor.backend.dto.AuthResponseDto;
+import com.teodor.backend.dto.BaseResponseDto;
+import com.teodor.backend.dto.UserCredentialsDto;
+import com.teodor.backend.entity.User;
+import com.teodor.backend.service.AuthService;
+import com.teodor.backend.service.JwtService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final JwtService jwtService;
+
+    private final AuthService authService;
+
+    public AuthController(JwtService jwtService, AuthService authService) {
+        this.jwtService = jwtService;
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<BaseResponseDto<AuthResponseDto>> login(@RequestBody @Valid UserCredentialsDto loginUserDto) {
+        BaseResponseDto<AuthResponseDto> baseResponseDto = new BaseResponseDto<>();
+
+        try {
+            User authenticatedUser = authService.loginUser(loginUserDto);
+
+
+            String jwtToken = jwtService.generateToken(authenticatedUser);
+
+            AuthResponseDto loginResponse = new AuthResponseDto(
+                    jwtToken,
+                    authenticatedUser.getUsername(),
+                    authenticatedUser.getId()
+            );
+
+            baseResponseDto.setSuccessResult(loginResponse);
+
+            return ResponseEntity.ok().body(baseResponseDto);
+        } catch (AuthenticationException e) {
+            baseResponseDto.setErrorMessage(e.getMessage());
+
+            return ResponseEntity.badRequest().body(baseResponseDto);
+        }
+    }
+}
