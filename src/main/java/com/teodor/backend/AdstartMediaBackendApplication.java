6930030faa2ac12edde3838da9a65887d36a2e01
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-20 10:43:43 +0300

    Initial project setup

---
+++ src/main/java/com/teodor/backend/AdstartMediaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teodor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdstartMediaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdstartMediaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teodor/backend/AdstartMediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teodor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdstartMediaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
